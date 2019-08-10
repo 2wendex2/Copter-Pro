a541b95c867389b9443fb8052a290c4c7e94221e
2wendex2 <2wendex2@mail.ru>
2019-08-10 21:50:14 +0300

    v0.1.1

--- cp/src/menu/Window.java
+++
@@ -1,9 +0,0 @@
-package menu;
-
-import control.ControlException;
-import control.Drawable;
-
-public interface Window extends Drawable {
-    void onExit();
-    void init() throws ControlException;
-}
